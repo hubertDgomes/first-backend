efa77943978494d30e048517f9e301a9096e49fc
hubertDgomes <gomeshubert8@gmail.com>
2025-11-15 22:29:24 +0600

    as

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-// No "ejs" needed to be imported or in a variable.
+// No "ejs" needed to be imported or in a variable.ss
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
