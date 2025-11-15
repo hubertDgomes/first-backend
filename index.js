cd85f246ec737ce4060e0e289e903e1237f40472
hubertDgomes <gomeshubert8@gmail.com>
2025-11-15 22:30:58 +0600

    3rd commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-// No "ejs" needed to be imported or in a variable.ss
+// No "ejs" needed to be imported or in a variable.
+// I just need to change.
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
